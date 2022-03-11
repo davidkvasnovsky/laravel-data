014cab83bef3651f601f92d347bd75b35f2a8f78
Ruben Van Assche <ruben@spatie.be>
2022-03-11 12:05:42 +0100

    wip

--- tests/Resolvers/DataFromSomethingResolverTest.php
+++ tests/Resolvers/DataFromSomethingResolverTest.php
@@ -4,6 +4,9 @@ namespace Spatie\LaravelData\Tests\Resolvers;
 
 use Illuminate\Contracts\Support\Arrayable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
 use Spatie\LaravelData\Data;
 use Spatie\LaravelData\Tests\Fakes\DummyDto;
 use Spatie\LaravelData\Tests\Fakes\DummyModel;
@@ -12,6 +15,15 @@ use Spatie\LaravelData\Tests\TestCase;
 
 class DataFromSomethingResolverTest extends TestCase
 {
+    public function setUp() : void
+    {
+        parent::setUp();
+
+        $this->handleExceptions([
+            ValidationException::class
+        ]);
+    }
+
     /** @test */
     public function it_can_create_data_from_a_custom_method()
     {
@@ -85,5 +97,53 @@ class DataFromSomethingResolverTest extends TestCase
         $inherited = new DummyModel(['string' => 'Rick Astley']);
 
         $this->assertEquals(new $data('Rick Astley'), $data::from($inherited));
+    }
+
+    /** @test */
+    public function it_can_create_data_from_a_custom_method_with_multiple_parameters()
+    {
+        $data = new class ('', 0) extends Data {
+            public function __construct(
+                public string $string,
+                public int $number,
+            ) {
+            }
+
+            public static function fromMultiple(string $first, int $second)
+            {
+                return new self($first, $second);
+            }
+        };
+
+        $this->assertEquals(new $data('Rick Astley', 42), $data::from(
+            'Rick Astley',
+            42,
+        ));
+    }
+
+    /** @test */
+    public function it_will_validate_a_request_when_given_as_a_parameter_to_a_custom_creation_method()
+    {
+        $data = new class ('', 0) extends Data {
+            public function __construct(
+                public string $string,
+            ) {
+            }
+
+            public static function fromRequest(Request $request)
+            {
+                return new self($request->input('string'));
+            }
+        };
+
+        Route::post('/', fn(Request $request) => $data::from($request));
+
+        $this->postJson('/', [])->assertJsonValidationErrorFor('string');
+
+        $this->postJson('/', [
+            'string' => 'Rick Astley',
+        ])->assertJson([
+            'string' => 'Rick Astley',
+        ])->assertOk();
     }
 }
